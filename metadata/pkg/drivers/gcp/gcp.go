c3235a193963d7ceb32d63a3f755e231df2562c1
Amit Debnath <amitdn2005@gmail.com>
2023-03-10 00:57:41 +0530

    Adding GetBucketMeta and ObjectList methods for metadata GCP driver

--- metadata/pkg/drivers/gcp/gcp.go
+++ metadata/pkg/drivers/gcp/gcp.go
@@ -16,16 +16,18 @@ package gcp
 
 import (
 	"context"
+	"strings"
 	"sync"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/credentials"
+	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	awss3 "github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/aws/session"
 	backendpb "github.com/opensds/multi-cloud/backend/proto"
 	"github.com/opensds/multi-cloud/metadata/pkg/db"
 	"github.com/opensds/multi-cloud/metadata/pkg/model"
+	"github.com/opensds/multi-cloud/metadata/pkg/utils"
 	pb "github.com/opensds/multi-cloud/metadata/proto"
 	log "github.com/sirupsen/logrus"
 	"github.com/webrtcn/s3client"
@@ -34,6 +36,129 @@ import (
 type GcpAdapter struct {
 	Backend *backendpb.BackendDetail
 	Session *s3client.Client
+}
+
+
+func (ad *GcpAdapter)ObjectList(sess *s3client.Client, bucket *model.MetaBucket) error {
+	Region := aws.String(ad.Backend.Region)
+	Endpoint := aws.String(ad.Backend.Endpoint)
+	Credentials := credentials.NewStaticCredentials(ad.Backend.Access, ad.Backend.Security, "")
+	configuration := &aws.Config{
+		Region:      Region,
+		Endpoint:    Endpoint,
+		Credentials: Credentials,
+	}
+
+	svc := awss3.New(session.New(configuration))
+	output, err := svc.ListObjectsV2(&s3.ListObjectsV2Input{Bucket: &bucket.Name})
+	if err != nil {
+		log.Errorf("unable to list objects in bucket %v. failed with error: %v", bucket.Name, err)
+		return err
+	}
+
+	numObjects := len(output.Contents)
+	var totSize int64
+	objectArray := make([]*model.MetaObject, numObjects)
+	for objIdx, object := range output.Contents {
+		obj := &model.MetaObject{}
+		objectArray[objIdx] = obj
+		obj.LastModifiedDate = object.LastModified
+		obj.ObjectName = *object.Key
+		obj.Size = *object.Size
+		totSize += obj.Size
+		obj.StorageClass = *object.StorageClass
+
+		tags, err := svc.GetObjectTagging(&s3.GetObjectTaggingInput{Bucket: &bucket.Name, Key: &obj.ObjectName})
+
+		if err == nil {
+			tagset := map[string]string{}
+			for _, tag := range tags.TagSet {
+				tagset[*tag.Key] = *tag.Value
+			}
+			obj.ObjectTags = tagset
+			if tags.VersionId != nil {
+				obj.VersionId = *tags.VersionId
+			}
+		} else {
+			log.Errorf("unable to get object tags. failed with error: %v", err)
+		}
+
+		acl, err := svc.GetObjectAcl(&s3.GetObjectAclInput{Bucket: &bucket.Name, Key: &obj.ObjectName})
+		if err != nil {
+			log.Errorf("unable to get object Acl. failed with error: %v", err)
+		} else {
+			access := []*model.Access{}
+			for _, grant := range acl.Grants {
+				access = append(access, utils.AclMapper(grant))
+			}
+			obj.ObjectAcl = access
+		}
+
+		//ad.GetHeadObject(sess, &bucket.Name, obj)
+	}
+	bucket.NumberOfObjects = numObjects
+	bucket.TotalSize = totSize
+	bucket.Objects = objectArray
+	return nil
+}
+
+func (ad *GcpAdapter)GetBucketMeta(buckIdx int, bucket *s3.Bucket, sess *s3client.Client, bucketArray []*model.MetaBucket, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	Region := aws.String(ad.Backend.Region)
+	Endpoint := aws.String(ad.Backend.Endpoint)
+	Credentials := credentials.NewStaticCredentials(ad.Backend.Access, ad.Backend.Security, "")
+	configuration := &aws.Config{
+		Region:      Region,
+		Endpoint:    Endpoint,
+		Credentials: Credentials,
+	}
+
+	svc := awss3.New(session.New(configuration))
+	loc, err := svc.GetBucketLocation(&s3.GetBucketLocationInput{Bucket: bucket.Name})
+	if err != nil {
+		log.Errorf("unable to get bucket location. failed with error: %v", err)
+		return
+	}
+
+	if *loc.LocationConstraint != ad.Backend.Region {
+		return
+	}
+
+	buck := &model.MetaBucket{}
+	buck.Name = *bucket.Name
+	buck.CreationDate = bucket.CreationDate
+	buck.Region = *loc.LocationConstraint
+
+	err = ad.ObjectList(sess, buck)
+	if err != nil {
+		return
+	}
+
+	bucketArray[buckIdx] = buck
+
+	tags, err := svc.GetBucketTagging(&s3.GetBucketTaggingInput{Bucket: bucket.Name})
+
+	if err == nil {
+		tagset := map[string]string{}
+		for _, tag := range tags.TagSet {
+			tagset[*tag.Key] = *tag.Value
+		}
+		buck.BucketTags = tagset
+	} else if !strings.Contains(err.Error(), "NoSuchTagSet") {
+		log.Errorf("unable to get bucket tags. failed with error: %v", err)
+	}
+
+	acl, err := svc.GetBucketAcl(&s3.GetBucketAclInput{Bucket: bucket.Name})
+	if err != nil {
+		log.Errorf("unable to get bucket Acl. failed with error: %v", err)
+	} else {
+		access := []*model.Access{}
+		for _, grant := range acl.Grants {
+			access = append(access, utils.AclMapper(grant))
+		}
+		buck.BucketAcl = access
+	}
 }
 
 func (ad *GcpAdapter) BucketList(sess *s3client.Client) ([]*model.MetaBucket, error) {
@@ -61,10 +186,18 @@ func (ad *GcpAdapter) BucketList(sess *s3client.Client) ([]*model.MetaBucket, er
 	for i, bucket := range output.Buckets {
 		log.Info("AMIT : I %d: bucket: %v", i, bucket)
 		wg.Add(1)
-		// go GetBucketMeta(i, bucket, sess, bucketArray, &wg)
+		go ad.GetBucketMeta(i, bucket, sess, bucketArray, &wg)
 	}
 	wg.Wait()
-	return bucketArray, err
+
+	bucketArrayFiltered := []*model.MetaBucket{}
+	for _, buck := range bucketArray {
+		if buck != nil {
+			bucketArrayFiltered = append(bucketArrayFiltered, buck)
+		}
+	}
+
+	return bucketArrayFiltered, err
 }
 
 func (ad *GcpAdapter) SyncMetadata(ctx context.Context, in *pb.SyncMetadataRequest) error {
